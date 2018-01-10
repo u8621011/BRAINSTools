463b7856e08a01db6d6528abd72ce669e0674744
Ted Chen <shapable.ted@gmail.com>
2018-01-10 17:15:20 +0800

    WIP: add i18n cmake scripts and some ts files.

---
+++ BRAINSFit/Resources/Translations/BRAINSFit_zh_tw.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="zh_TW">
+<context>
+    <name>BRAINSFit</name>
+    <message>
+        <source>Fixed Image Volume</source>
+        <translation>固定的圖片</translation>
+    </message>
+    <message>
+        <source>Input fixed image (the moving image will be transformed into this image space).</source>
+        <translation>固定的圖片(移動的圖片會被轉換到這個圖片的空間內)</translation>
+    </message>
+
+    <message>
+        <source>movingVolume</source>
+        <translation>移動的圖片</translation>
+    </message>
+
+</context>
+</TS>
